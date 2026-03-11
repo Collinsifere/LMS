e41a80dbb23b33107f0a90d47eb99891558b85f7
Collins Ifere <55281071+Collinsifere@users.noreply.github.com>
2026-03-11 17:56:05 +0000

    chore(db): add Flask-Migrate and initialize Alembic migrations

---
+++ migrations/versions/de870e286ec2_baseline.py
@@ -0,0 +1,24 @@
+"""baseline
+
+Revision ID: de870e286ec2
+Revises: 
+Create Date: 2026-03-11 14:40:47.395086
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'de870e286ec2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
